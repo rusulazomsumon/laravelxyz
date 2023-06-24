e38511b426c7274df059977ea90f29a6c67685ea
RusulAzomSumon <18103020@iubat.edu>
2023-06-24 23:57:27 +0600

    contat page updated

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -8,7 +8,7 @@
                     <div class="card-header">Contacts</div>
                     <div class="card-body">
                         <a href="{{ url('/contact/create') }}" class="btn btn-success btn-sm" title="Add New Contact">
-                            <i class="fa fa-plus" aria-hidden="true"></i> Add New
+                            <i class="fa fa-plus" aria-hidden="true"></i> Add New Student
                         </a>
                         <br/>
                         <br/>
